072a027401b25aee36d9727af2abd5e20f93af0f
Erik Pernod <erik.pernod@gmail.com>
2016-10-02 13:15:30 +0200

    ADD: all default config files and empty source from qx-sdk/create-application.py

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+// requires
+var util = require('util');
+var qx = require("../../../qooxdoo-sdk/tool/grunt");
+
+// grunt
+module.exports = function(grunt) {
+  var config = {
+
+    generator_config: {
+      let: {
+      }
+    },
+
+    common: {
+      "APPLICATION" : "qxthree",
+      "QOOXDOO_PATH" : "../../../qooxdoo-sdk",
+      "LOCALES": ["en"],
+      "QXTHEME": "qxthree.theme.Theme"
+    }
+
+    /*
+    myTask: {
+      options: {},
+      myTarget: {
+        options: {}
+      }
+    }
+    */
+  };
+
+  var mergedConf = qx.config.mergeConfig(config);
+  // console.log(util.inspect(mergedConf, false, null));
+  grunt.initConfig(mergedConf);
+
+  qx.task.registerTasks(grunt);
+
+  // grunt.loadNpmTasks('grunt-my-plugin');
+};
---
+++ source/class/qxthree/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("qxthree.theme.Theme",
+{
+  meta :
+  {
+    color : qxthree.theme.Color,
+    decoration : qxthree.theme.Decoration,
+    font : qxthree.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : qxthree.theme.Appearance
+  }
+});
