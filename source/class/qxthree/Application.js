686eb16853418bffa7113d7208345064a512b14e
epernod <erik.pernod@belcurves.com>
2016-10-05 01:10:43 +0200

    UPDATE: clean GLRenderer to use right widget size. Remove arguments from the constructor. FIX: the display of Three inside a widget. ADD: animation loop and cube texture.

--- source/class/qxthree/Application.js
+++ source/class/qxthree/Application.js
@@ -14,7 +14,7 @@
  */
 qx.Class.define("qxthree.Application",
 {
-    extend: qx.application.Inline,
+  extend : qx.application.Standalone,
 
 
   /*
@@ -25,8 +25,6 @@ qx.Class.define("qxthree.Application",
 
   members :
   {
-      __webElement: null,
-      __glRenderer: null,
     /**
      * This method contains the initial application code and gets called 
      * during startup of the application
@@ -56,113 +54,22 @@ qx.Class.define("qxthree.Application",
       // Document is the application root
       var doc = this.getRoot();
 
-      this.__webElement = document.getElementById("qooxdooDiv");
+      // Create Gl Canvas
+      var glRenderer = new qxthree.GLRenderer();
       
-      if(this.__webElement)
-          this.__initDiv();
-            
-      
-//      this.glRenderer = new qxthree.GLRenderer();
-     // win.add(glRenderer.getRenderer());
-          
-  //    this.glRenderer.testMethod();
-
-    
-      
-    },
-    
-    __initDiv: function()
-    {
-        var widthDiv = this.__webElement.getAttribute("width");
-        var heightDiv = this.__webElement.getAttribute("height");
-                              
-        if (widthDiv)
-            this.__webElement.style.width = widthDiv + "px";
-        else {
-            widthDiv = this.__webElement.clientWidth;
-            
-            if (widthDiv == null || widthDiv < 100)
-                widthDiv = 800;
-            
-            this.__webElement.style.width = widthDiv + "px";
-        }
-            
-        if (heightDiv)
-            this.__webElement.style.height = heightDiv + "px";
-        else {
-            heightDiv = this.__webElement.clientHeight;
-            
-            if (heightDiv == null || heightDiv < 100)
-                heightDiv = 600;
-            
-            this.__webElement.style.height = heightDiv + "px";
-        }
-               
-        this.debug("widthDiv: " + widthDiv);
-        this.debug("heightDiv: " + heightDiv);
-
-               
-        this.__inlineModule = new qx.ui.root.Inline(this.__webElement, true, true);
-        this.__inlineModule.addListener("resize", this.__updateSize, this);
-        this.__inlineModule.set({
-            backgroundColor : "blue"
-        });
-        
-        this.__glRenderer = new qxthree.GLRenderer(widthDiv, heightDiv, this);
-        this.__glRenderer.addListener("sceneCreated", this.create3DScene, this);    
-        //this.__glRenderer.init();
-
-        
-        var box = new qx.ui.container.Composite(new qx.ui.layout.VBox(8));
-        box.set({
-            backgroundColor : "blue",
-                    width : 500,
-                    allowShrinkY : false,
-                    allowShrinkX : false,
-                    height : 500
-        });
-        
-        this.__inlineModule.add(this.__glRenderer);
-        //this.__inlineModule.add(box);
-    },
-    
-    
-    __updateSize : function(event)
-    {        
-        this.debug("app::updateSize");
-        var width = parseInt(qx.bom.element.Style.get(this.__webElement, 'width'));
-        var height = parseInt(qx.bom.element.Style.get(this.__webElement, 'height'));
-        
-        this.debug("width: " + width);
-        this.debug("height: " + height);
-      
-        if(this.__mainWindow) {
-            this.__mainWindow.setWidth(width);
-            this.__mainWindow.setHeight(height);    
-        }
-        
-    },
-
-    create3DScene : function(){
-        
-        
-        
-        this.debug("create3DScene");
-       // return;
-        
-        
-       
-
-       // document.body.appendChild(this.__glRenderer.getRenderer());
-        //win.add(chk);
-        
-//        var test = document.getElementById("qooxdooDiv");
-//        if(test)
-//            test.appendChild(this.glRenderer.getRenderer());
-// 
-//        if(chk)
-//                chk.appenchild(this.glRenderer.getRenderer());
-
+      var win = new qx.ui.window.Window('Three 3D Cube example').set(
+              {
+                  backgroundColor: "red",
+                  width : 500,
+                  height : 500
+              });
+      win.setLayout(new qx.ui.layout.Grow());
+      win.addListener('appear', function() {
+          win.center()
+      });
+      win.add(glRenderer);
+      win.open();
     }
+
   }
 });
--- source/class/qxthree/GLRenderer.js
+++ source/class/qxthree/GLRenderer.js
@@ -20,199 +20,109 @@ qx.Class.define("qxthree.GLRenderer", {
     extend : qx.ui.core.Widget,
     include: [qxthree.MixinGLRenderer],
 
-    construct : function(W, H, parent)
+    construct : function()
     {
         this.base(arguments);
-        this.__width = W;
-        this.__height = H;
-        this.__parent = parent;
-        
-        this.set(
-                {
-                    backgroundColor: "green",
-//                    canvasWidth: W,
-//                    canvasHeight: H,
-//                    syncDimension: false,
-                    width: W,
-                    height: H
-                });
-       
-        this.addListener("scriptLoaded", this.__initScene, this);
-        this.addListener("track", this.__onTrack, this);
-        this.addListener("appear", this.__webGLStart, this);
-        
-//        this.addListenerOnce('scriptLoaded',function(e){
-//            var el = this.getContentElement().getDomElement();
-//            
-//            var scene = new THREE.Scene();
-//            var camera = new THREE.PerspectiveCamera( 75, this.getBounds().width/this.getBounds().height, 0.1, 1000 );
-//
-//            var renderer = new THREE.WebGLRenderer();
-//            renderer.setSize( this.getBounds().width,this.getBounds().height );
-//            el.appendChild(renderer.domElement);
-//      
-//            var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-//            var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-//            var cube = new THREE.Mesh( geometry, material );
-//            scene.add( cube );
-//      
-//            camera.position.z = 5;
-//      
-//            var render = function () {
-//            requestAnimationFrame( render );
-//              cube.rotation.x += 0.1;
-//              cube.rotation.y += 0.1;
-//              renderer.render(scene, camera);
-//            };
-//      
-//            render();
-//        },this);
-        
 
+        // Three.js scripts need to be loaded first. This will fired event scriptLoaded 
+        this.__setup();
+        
+        // Method to init the scene as soon as Three.js has been loaded
+        this.addListener("scriptLoaded", this.__initScene, this);
+        
+        // Others listeners
+        this.addListener("track", this.__onTrack, this);
     },
-    
+
     events : {
         sceneCreated: 'qx.event.type.Event'
     },
 
     members : {
         
-        __height: 400,
-        __width: 400,
+        __canvasHeight: 400,
+        __canvasWidth: 400,
         
         __camera: null,
         __scene: null,
         __renderer: null,
         __mesh: null,
-        __logEvents: true,
+        __logEvents: false,
 
-        __webGLStart: function(){
-            this.__setup();
-            this.debug("setup END");
-        },
-        
-        __initGL: function()
-        {
-            this.debug("GLRenderer::__initGL");
-            qx.ui.core.queue.Manager.flush();
-            this.canvasElement = this.getContentElement().getDomElement();
-            if (!this.canvasElement){
-                this.debug("Error: qxthree.GLRenderer: no DomElement found.")
-//                return false;
-            }
-            else
-                this.debug("cette fois ci c'est ok");
-            
-            
-        },
         
         __initScene: function()
         {
-            this.debug("GLRenderer::init");
+            this.debug("GLRenderer::__initScene");
             
             var el = this.getContentElement().getDomElement();
-            if (!this.canvasElement){
+            if (!el){
                 this.debug("Error: qxthree.GLRenderer: no DomElement found.")
-//                return false;
+                return false;
             }
             else
                 this.debug("cette fois ci c'est ok");
-         
-            
+                    
+            this.__canvasHeight = this.getBounds().height;
+            this.__canvasWidth = this.getBounds().width;
 
-            this.__camera = new THREE.PerspectiveCamera( 70, this.__width / this.__height, 1, 1000 );
+            this.__camera = new THREE.PerspectiveCamera( 70, this.__canvasWidth / this.__canvasHeight, 1, 1000 );
             this.__camera.position.z = 400;
             
             this.__scene = new THREE.Scene();
             
             var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
-            var material = new THREE.MeshBasicMaterial( { color: Math.random() * 0xffffff } );
+            
+            var texture = new THREE.TextureLoader().load( 'resource/crate.gif' );
+            
+            //var material = new THREE.MeshBasicMaterial( { color: Math.random() * 0xffffff } );
+            var material = new THREE.MeshBasicMaterial( { map: texture } );
             
             this.__mesh = new THREE.Mesh( geometry, material );
             this.__scene.add( this.__mesh );
             
             this.__renderer = new THREE.WebGLRenderer();
             this.__renderer.setPixelRatio( 1 );
-            this.__renderer.setSize( this.__width, this.__height );
+            
+            this.debug("this.__width: " + this.getBounds().width);
+            this.__renderer.setSize( this.__canvasWidth, this.__canvasHeight );
             
             el.appendChild( this.__renderer.domElement );
             
-            this.__renderer.render( this.__scene, this.__camera );
-            
-            /*
-new MeshBasicMaterial( { color: Math.random() * 0xffffff })
-                var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
-                var material = new THREE.MeshBasicMaterial( { map: texture } );
-                
-                document.body.appendChild( renderer.domElement );
-
-                //
-
-                window.addEventListener( 'resize', onWindowResize, false );
-*/
-            this.__animateTest();
-            
             this.fireDataEvent('sceneCreated');
+            this.animate();         
         },
         
         getRenderer: function(){
-            var element =this.__renderer.domElement; 
             return this.__renderer.domElement;
         },
-        
-        helloWorld : function ()
-        {
-            var label1 = new qx.ui.basic.Label("Hellow World from GLRender").set({
-                width: 200
-              });
-            
-            return label1;
-        },
-        
-        testMethod: function()
-        {
-            this.canvasElement = this.getContentElement().getDomElement();
-            if (!this.canvasElement){
-                this.debug("Error: qxthree.GLRenderer: no DomElement found.")
-                return false;
-            }
-            else
-                this.debug("Ok: qxthree.GLRenderer: DomElement found.")
-        },
-        
-        update: function(){
-            this.debug("qxthree.GLRenderer::update");
-        },
-        
+               
         __onTrack: function(trackEvent){
             if (qx.core.Environment.get("qx.debug") && this.__logEvents){
                 this.debug("Event: webGLController::__onTrack");
             }
-//
-//            requestAnimationFrame( animate );
-//
             this.__mesh.rotation.x += 0.005;
             this.__mesh.rotation.y += 0.01;
 
-            this.__renderer.render( this.__scene, this.__camera );
+            this.updateGL();
             
         },
         
-        __animateTest: function()
+        animate: function()
         {
-            //this.debug("qxthree.GLRenderer::__animateTest");
-            //requestAnimationFrame( this.__animateTest );
-//            return function(){
-//                var render = function(){
-//                    requestAnimationFrame( render );
-//                    this.__mesh.rotation.x += 0.005;
-//                    this.__mesh.rotation.y += 0.01;
-//
-//                    this.__renderer.render( this.__scene, this.__camera );
-//                };
-//            };
+          this.__mesh.rotation.x += 0.005;
+          this.__mesh.rotation.y += 0.01;
+
+          this.updateGL();
+
+          requestAnimationFrame( this.animate.bind(this) );                      
+        },
+        
+        updateGL: function()
+        {
+            if(!this.__renderer)
+                return;
             
-            
+            this.__renderer.render( this.__scene, this.__camera );
         }
     }
 });
