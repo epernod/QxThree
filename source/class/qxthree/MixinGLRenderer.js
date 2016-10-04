2b8ad82a1b2425b2140f5e5e824bb9f856eba11a
epernod <erik.pernod@belcurves.com>
2016-10-04 15:01:47 +0200

    ADD: first code similar to qxJQPlot to load sthird party scripts

--- source/class/qxthree/MixinGLRenderer.js
+++ source/class/qxthree/MixinGLRenderer.js
@@ -16,6 +16,7 @@
 /* ************************************************************************
 
 #asset(three/*)
+#ignore(THREE)
 
 ************************************************************************ */
 
@@ -26,16 +27,106 @@ qx.Mixin.define("qxthree.MixinGLRenderer", {
 
     statics : 
     {
-        INSTANCE_COUNTER : 0
+        INSTANCE_COUNTER : 0,
+        /**
+         * map of loaded scripts
+         */
+        LOADED: {},
+        /**
+         * map of objects in the process of loading a particular script
+         */
+        LOADING: {},
+        /**
+         * Default Options for graphs. They get merged (non overwriting to the graph object).
+         */
+        DEFAULT_OPTIONS: 
+        {
+            
+        }
+        
     },
     
     events :
     {
-    
+        /**
+         * fires when a script is loaded
+         */
+        scriptLoaded: 'qx.event.type.Event'
     },
     
     members :
-    {
+    {       
+        __animate: function()
+        {
+            
+        },
         
+        __setup: function()
+        {
+            var min = '.min';
+            if (qx.core.Environment.get("qx.debug")) {
+              min = '';
+            }
+            /* I guess it would not be all that difficult to create a stripped
+             * down jQuery object with all the bits required by jqPlot and use
+             * this instead of full jQuery.
+             */
+            var codeArr = [
+                  "three" + min + ".js"
+            ];
+            this.__loadScriptArr(codeArr, qx.lang.Function.bind(this.__addCanvas));
+        },
+        
+        __addCanvas:function (){
+            
+        },
+        
+        __loadScriptArr: function(codeArr,handler)
+        {
+            var script = codeArr.shift();
+            if (!script){
+                handler();
+                return;
+            }
+            
+            if (qxthree.MixinGLRenderer.LOADED[script]){
+                /* got this script, go for the next in the array */
+                 this.__loadScriptArr(codeArr,handler);
+                 return;
+            }
+            if (qxthree.MixinGLRenderer.LOADING[script]){
+                /* not loaded but it is already loading, so let's listen for it to arrive */
+                qxthree.MixinGLRenderer.LOADING[script].addListenerOnce('scriptLoaded',function(){
+                    this.__loadScriptArr(codeArr,handler);
+                },this);
+                return;
+            }
+            qxthree.MixinGLRenderer.LOADING[script] = this;
+
+            var src = qx.util.ResourceManager.getInstance().toUri("three/"+script);
+            if (qx.io.ScriptLoader){
+                var sl = new qx.io.ScriptLoader();
+                sl.load(src, function(status){
+                    if (status == 'success'){
+                        // this.debug("Dynamically loaded "+src+": "+status);
+                        qxthree.MixinGLRenderer.LOADED[script] = true;
+                        delete qxthree.MixinGLRenderer.LOADING[script];
+                        this.fireDataEvent('scriptLoaded',script);
+                        this.__loadScriptArr(codeArr,handler);
+                    }
+                },this);
+            }
+            else {
+                var req = new qx.bom.request.Script();
+                req.on('load',function() {
+                    qxthree.MixinGLRenderer.LOADED[script] = true;
+                    delete qxthree.MixinGLRenderer.LOADING[script];
+                    this.fireDataEvent('scriptLoaded',script);
+                    this.__loadScriptArr(codeArr,handler);
+                },this);
+                req.open("GET", src);
+                req.send();
+            }
+        }
     }
 });
